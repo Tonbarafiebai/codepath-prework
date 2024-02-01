43b7dfad5e6372baf5dfd331b081b27a7d91918a
Tonbarafiebai <nathanfiebai@gmail.com>
2024-01-31 21:39:59 -0500

    Added Prework files to repository

---
+++ Fiebai Application/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Fiebai Application
+//
+//  Created by Tonbara Fiebai on 1/31/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func changeBackgroundColor(_ sender: UIButton) {
+        func changeColor() -> UIColor{
+
+                let red = CGFloat.random(in: 0...1)
+                let green = CGFloat.random(in: 0...1)
+                let blue = CGFloat.random(in: 0...1)
+
+                return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+            }
+        let randomColor = changeColor()
+        view.backgroundColor = randomColor
+    }
+    
+}
+
